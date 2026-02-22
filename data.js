db2717078d7e5672ac6106496f44530891f337c7
fedexd-dlv <brisibemoses2000@gmail.com>
2026-02-22 08:24:17 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+var shipments = {
+
+"FDX123456789": {
+status: 2,
+location: "New York, USA",
+receiver: "John Williams",
+weight: "2.4kg",
+service: "Priority Express",
+eta: "March 3, 2026"
+},
+
+"FDX555888222": {
+status: 4,
+location: "Lagos, Nigeria",
+receiver: "Michael Ade",
+weight: "1.1kg",
+service: "International Economy",
+eta: "February 25, 2026"
+}
+
+};
